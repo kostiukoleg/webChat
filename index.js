26f199ff907921ca97b337b4d2278cb6381ae8a3
kostiukoleg <olegkostiuk@ukr.net>
2015-03-24 15:13:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by okostiuk on 24.03.15.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('index.html',{ root:__dirname});
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
